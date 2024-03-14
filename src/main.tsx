810316f3f4db3b1406ae3f73c9ac8be9073b41e6
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2024-03-14 15:18:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import App from "./App.js";
 import { ChakraProvider } from "@chakra-ui/react";
 import Welcome from "./components/Welcome.js";
@@ -11,7 +11,7 @@ import PageNotFound from "./components/page404.js";
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
       <Routes>
       <Route exact path="/" element={<Welcome />} />
       </Routes>
