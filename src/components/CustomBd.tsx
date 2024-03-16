8d30b102480e8aee488b815abdfc2aeca8b64572
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2024-03-16 17:39:40 +0530

    Productional Commit for Phones

--- src/components/CustomBd.tsx
+++ src/components/CustomBd.tsx
@@ -1,22 +1,22 @@
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 const CustomBreadcrumb = () => {
   return (
-    <Breadcrumb  separator={<ChevronRightIcon color='gray.500' />}>
+    <Breadcrumb separator={<ChevronRightIcon color="gray.500" />}>
       <BreadcrumbItem>
-        <BreadcrumbLink as={Link} to='/'>
+        <BreadcrumbLink as={Link} to="/#">
           Home
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbItem>
-        <BreadcrumbLink as={Link} to='/page1'>
+        <BreadcrumbLink as={Link} to="/page1">
           Release Notes
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbItem>
-        <BreadcrumbLink as={Link} to='/page2'>
+        <BreadcrumbLink as={Link} to="/page2">
           Page 2
         </BreadcrumbLink>
       </BreadcrumbItem>
